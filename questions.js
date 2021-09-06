d58a8fd58b54fc6e2afb6fbc51be484ca35b4dd7
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-09-06 20:12:19 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:"Your English is very fluent. I wish I could speak ..... you.",
+        options:["like", "as"],
+        answer: 0
+    },
+    {
+        q:"You needn't take my advise if you don't want to. You can do ..... you like.",
+        options:["like", "as"],
+        answer: 1
+    },
+    {
+        q:"He wastes too much time doing things ..... sitting in cafes all day.",
+        options:["like", "as"],
+        answer: 0
+    },
+    {
+        q:"There is no need to change your clothes. You can go out ..... you are.",
+        options:["like", "as"],
+        answer: 1
+    },
+    {
+        q:"The weather's terrible for the middle of summer. It's ..... winter.",
+        options:["like", "as"],
+        answer: 0
+    },
+    {
+        q:"He's decided to give up his job ..... a journalist and become a teacher.",
+        options:["like", "as"],
+        answer: 1
+    },
+    {
+        q:"I think I prefer this room ..... it was, before we decorated it.",
+        options:["as", "like"],
+        answer: 0
+    },
+    {
+        q:"When we asked Jack to help us, he agreed immediatelly, ..... I knew he would.",
+        options:["like", "as"],
+        answer: 1
+    },
+    {
+        q:"While we were on holiday, we spent most of our time doing energetic things ..... sailing, water-skiing and swimming.",
+        options:["like", "as"],
+        answer: 0
+    },
+    {
+        q:"Ann's been working ..... a waitress for the last few weeks.",
+        options:["like", "as"],
+        answer: 1
+    }
+
+
+
+]
